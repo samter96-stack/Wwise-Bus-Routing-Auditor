8fb31c18085b45de0565146320cf7a3f9f8a1e98
samter96 <samter96@gmail.com>
2026-04-13 19:12:09 +0900

    Update install_addon.bat

--- install_addon.bat
+++ install_addon.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
+
 cd /d "%~dp0"
 
 :: launch.bat 절대 경로 계산
@@ -9,9 +10,9 @@ set "LAUNCH_PATH=%~dp0launch.bat"
 set "ADDON_DIR=%APPDATA%\Audiokinetic\Wwise\Add-ons\Commands"
 if not exist "%ADDON_DIR%" mkdir "%ADDON_DIR%"
 
-:: JSON 내 백슬래시 이스케이프 (PowerShell 에 위임)
-powershell -NoProfile -Command ^
-    "$launch = '%LAUNCH_PATH:\=\\%';" ^
+:: JSON 내 백슬래시 이스케이프 (PowerShell 에 위임) - 전체 경로 사용
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -Command ^
+    "$launch = '%LAUNCH_PATH:\\=\\\\%';" ^
     "$json = ConvertTo-Json @{version=1; commands=@(@{id='com.tools.bus-routing-auditor'; displayName='Bus Routing Auditor'; program=$launch; mainMenu=@{basePath='Tools'}})} -Depth 5;" ^
     "Set-Content -Path '%ADDON_DIR%\BusRoutingAuditor.json' -Value $json -Encoding UTF8"
 
@@ -30,3 +31,4 @@ echo   메뉴 ^> Tools ^> Reload Command Add-ons
 echo 또는 Wwise 를 재시작하면 Tools 메뉴에 "Bus Routing Auditor" 가 나타납니다.
 echo.
 pause
+
